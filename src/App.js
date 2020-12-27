73cac850a12ae4b7d4cfa698d7ba2c061067532b
Anas Al Kala <aal1580@students.fhv.at>
2020-12-27 10:42:09 +0100

    first step

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {CardList} from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component'
 import { SearchBox } from './components/search-box/search-box.component'
 // import logo from './logo.svg';
 import './App.css';
@@ -20,9 +20,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
-  handleChange=(e) => {
+  handleChange = (e) => {
     this.setState({ searchField: e.target.value });
   }
+
 
   render() {
     const { monsters, searchField } = this.state;
@@ -33,11 +34,12 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
         />
-        <CardList monsters={filteredMonsters}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
